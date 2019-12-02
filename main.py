8a18b00eb8677bd268473df8491332286f4c4979
Jeroendevr <jeroendevries@runbox.eu>
2019-12-02 16:22:12 +0100

    Adjusted for 2020
    The prices differences(taxes) and adjustment for CV efficiency. Based on information provided https://energycircle.nl/energiebelasting-ode-2019-en-de-prognose-voor-2020/ with BTW(VAT) of 21%

--- main.py
+++ main.py
@@ -6,22 +6,22 @@
 def gas_energy() -> float:
     # Total energy in kW/h in a m3 gas
     gas_energy = 9.8
-    CV_efficiency = 0.905
+    CV_efficiency = 0.944
     return gas_energy * CV_efficiency
 
 def electricity_price() :
     # Total price for one kW/h
     price_elec_levering = 0.07156
-    price_elec_ODE = 0.02287
-    price_elec_EB = 0.11934
+    price_elec_ODE = 0.030371
+    price_elec_EB = 0.121726
 
     return price_elec_EB + price_elec_ODE + price_elec_levering
 
 def gas_price():
     # Total price for one m3
     price_gas_levering = 0.27350
-    price_gas_EB = 0.35469
-    price_gas_ODE = 0.06340
+    price_gas_EB = 0.3617779
+    price_gas_ODE = 0.084337
 
     return price_gas_levering + price_gas_EB + price_gas_ODE
 
